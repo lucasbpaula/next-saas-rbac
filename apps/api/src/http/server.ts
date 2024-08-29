b18d28b71eedcd63ff1f17d407470eb91abb6e6c
lbpaula <lbpaula@brasilseg.com.br>
2024-08-29 16:59:20 -0300

    feat(backend): add authentication scheme in swagger

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -2,6 +2,7 @@ import { fastifyCors } from '@fastify/cors'
 import fastifyJwt from '@fastify/jwt'
 import fastifySwagger from '@fastify/swagger'
 import fastifySwaggerUI from '@fastify/swagger-ui'
+import { env } from '@saas/env'
 import { fastify } from 'fastify'
 import {
   jsonSchemaTransform,
@@ -34,8 +35,18 @@ app.register(fastifySwagger, {
       description: 'Fullstack Saas app with multi-tenant & RBAC',
       version: '1.0.0',
     },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
     servers: [],
   },
+
   transform: jsonSchemaTransform,
 })
 
@@ -45,7 +56,7 @@ app.register(fastifySwaggerUI, {
 
 // JWT
 app.register(fastifyJwt, {
-  secret: 'my-jwt-secret',
+  secret: env.JWT_SECRET,
 })
 
 // CORS
@@ -59,6 +70,6 @@ app.register(getProfile)
 app.register(requestPasswordRecover)
 app.register(resetPassword)
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP Server running!')
 })
