26507595db072394198293e3a4844369024984bc
lbpaula <lbpaula@brasilseg.com.br>
2024-08-29 18:01:22 -0300

    feat(backend): add get-membership route

---
+++ apps/api/src/http/routes/orgs/get-membership.ts
@@ -0,0 +1,47 @@
+import { roleSchema } from '@sass/auth'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+
+export async function getMembership(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug/membership',
+      {
+        schema: {
+          tags: ['organizations'],
+          summary: 'Get users membership from a organization',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              membership: z.object({
+                id: z.string().uuid(),
+                role: roleSchema,
+                organizationId: z.string().uuid(),
+              }),
+            }),
+          },
+        },
+      },
+      async (request) => {
+        const { slug } = request.params
+
+        const { membership } = await request.getUserMembership(slug)
+
+        return {
+          membership: {
+            id: membership.id,
+            role: membership.role,
+            organizationId: membership.organizationId,
+          },
+        }
+      },
+    )
+}
