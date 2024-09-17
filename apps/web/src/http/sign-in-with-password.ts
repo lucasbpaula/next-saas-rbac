9204a1afc86178f68a674dcec6a032902efb5718
lbpaula <lbpaula@brasilseg.com.br>
2024-09-17 17:20:33 -0300

    feat(front): create signInWithPassword action

---
+++ apps/web/src/http/sign-in-with-password.ts
@@ -0,0 +1,26 @@
+import { api } from './api-client'
+
+interface ISignInWithPasswordRequest {
+  email: string
+  password: string
+}
+
+interface ISignInWithPasswordResponse {
+  token: string
+}
+
+export async function signInWithPassword({
+  email,
+  password,
+}: ISignInWithPasswordRequest): Promise<ISignInWithPasswordResponse> {
+  const result = await api
+    .post('sessions/password', {
+      json: {
+        email,
+        password,
+      },
+    })
+    .json<ISignInWithPasswordResponse>()
+
+  return result
+}
