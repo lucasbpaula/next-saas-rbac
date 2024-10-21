08e87c6a9d65490d8024b6146b8bcf973b55a5f3
lbpaula <lbpaula@brasilseg.com.br>
2024-10-21 14:16:18 -0300

    feat(front): adds accept invite route and get pending invite popover

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -5,6 +5,7 @@ import { cookies } from 'next/headers'
 import { z } from 'zod'
 
 import { signInWithPassword } from '@/http/sign-in-with-password'
+import { acceptInvite } from '@/http/accept-invite'
 
 const signInSchema = z.object({
   email: z
@@ -34,6 +35,15 @@ export async function signInWithEmailAndPassword(formData: FormData) {
       path: '/',
       maxAge: 60 * 60 * 24 * 7, // 7 days
     })
+
+    const inviteId = cookies().get('inviteId')?.value
+
+    if (inviteId) {
+      try {
+        await acceptInvite(inviteId)
+        cookies().delete('inviteId')
+      } catch {}
+    }
   } catch (err) {
     if (err instanceof HTTPError) {
       const { message } = await err.response.json()
